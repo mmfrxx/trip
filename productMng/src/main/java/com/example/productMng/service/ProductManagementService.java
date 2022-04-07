71a7c6a67cd8508e380ab48b20f30d3b3cae536f
Art <a.zylinski@students.uu.nl>
2022-04-07 15:50:07 +0200

    added product management service

---
+++ productMng/src/main/java/com/example/productMng/service/ProductManagementService.java
@@ -0,0 +1,4 @@
+package com.example.productMng.service;
+
+public class ProductManagementService {
+}
---
+++ productMng/src/test/java/com/example/productMng/ProductMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productMng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
